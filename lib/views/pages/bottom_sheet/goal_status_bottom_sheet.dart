5af59327df819e5a273da29ea52ea7ac91e693ab
zahid0162 <zaidmuneer25@gmail.com>
2026-04-27 09:21:54 +0300

    sheets

---
+++ lib/views/pages/bottom_sheet/goal_status_bottom_sheet.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_happy_work_place/models/enum/gender_enum.dart';
+import 'package:my_happy_work_place/models/enum/goal_type_enum.dart';
+
+import '../../../constants/app_constants.dart';
+import '../../../constants/color_constants.dart';
+import '../../../constants/spacing.dart';
+import '../../widgets/custom_button.dart';
+import '../../widgets/custom_text.dart';
+
+class GoalStatusBotttomSheet extends StatefulWidget {
+  GoalTypes? selectedType;
+
+  GoalStatusBotttomSheet({super.key, this.selectedType});
+
+  @override
+  State createState() => _CountrySelectorBottomSheetState();
+}
+
+class _CountrySelectorBottomSheetState extends State<GoalStatusBotttomSheet> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StatefulBuilder(
+      builder: (BuildContext context, StateSetter setModalState) {
+        return Padding(
+          padding: EdgeInsets.only(
+            top: Spacing.normal,
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 52.w,
+                height: 5.h,
+                decoration: BoxDecoration(
+                  color: ColorConstants.bottomSheetIndicator,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              SizedBox(
+                height: Spacing.standard,
+              ),
+              CustomText(
+                  text: AppConstants.setGoalStatus,
+                  weight: FontWeight.w600,
+                  size: AppConstants.font16Px),
+              SizedBox(
+                height: Spacing.standard,
+              ),
+              SizedBox(
+                height: Spacing.standard,
+              ),
+              gendersView(setModalState),
+              Container(
+                padding: EdgeInsets.symmetric(
+                    vertical: Spacing.xxxxlarge, horizontal: Spacing.standard),
+                decoration: BoxDecoration(
+                  color: ColorConstants.homeScreenBackground,
+                  boxShadow: [
+                    BoxShadow(
+                      color: ColorConstants.bottomTileColor,
+                      spreadRadius: 3, // Spread radius
+                      blurRadius: 6, // Blur radius
+                      offset: Offset(0, -3),
+                    ),
+                  ],
+                ),
+                child: CustomButton(
+                  onTap: () {
+                    Navigator.pop(context, widget.selectedType);
+                  },
+                  title: AppConstants.done,
+                  radius: AppConstants.defaultButtonRadius,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget gendersView(StateSetter setModalState) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: Spacing.xxlarge*2, horizontal: Spacing.standard),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          singleGender(GoalTypes.ACTIVE,setModalState),
+          singleGender(GoalTypes.COMPLETED, setModalState),
+          singleGender(GoalTypes.GIVEUP, setModalState)
+        ],
+      ),
+    );
+  }
+
+  Widget singleGender(GoalTypes type,StateSetter setModalState){
+    return GestureDetector(
+      onTap: (){
+        setModalState(() {
+          widget.selectedType = type;
+        });
+        setState(() {
+          widget.selectedType = type;
+        });
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: Spacing.normal, horizontal: Spacing.xxxlarge + 2),
+        decoration: BoxDecoration(
+            color: widget.selectedType == type
+                ? ColorConstants.genderSelectedBgColor
+                : ColorConstants.genderUnselectedBgColor,
+            borderRadius:
+            BorderRadius.circular(AppConstants.defaultRadius + 4)),
+        child: CustomText(
+          text: type.name,
+          textColor: widget.selectedType == type
+              ? ColorConstants.primary
+              : ColorConstants.blackInputText,
+          align: TextAlign.center,
+          weight: FontWeight.w500,
+          size: AppConstants.font14Px,
+        ),
+      ),
+    );
+  }
+}
